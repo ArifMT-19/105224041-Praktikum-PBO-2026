476287abe18b1beabb1b8cbcee5800721f22a701
Arif Mufti Tharsa <105224041@student.universitaspertamina.ac.id>
2026-04-02 14:54:15 +0700

    Perbaiki Struktur Folder Modul-04

--- Modul-04/Modul-01/Latihan/src/App.java
+++
@@ -1,6 +0,0 @@
-public class App { 
-    public static void main(String[] args) throws Exception {
-        System.out.println("Praktikum Pemrograman Berorientasi Objek");
-        System.out.println("105224041");
-    }
-}
