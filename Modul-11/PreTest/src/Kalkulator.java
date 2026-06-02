135139cd50dee6820de634f19cd93fc02c3bcb77
Arif Mufti Tharsa <105224041@student.universitaspertamina.ac.id>
2026-06-02 14:24:25 +0700

    PreTest Modul 11

---
+++ Modul-11/PreTest/src/Kalkulator.java
@@ -0,0 +1,5 @@
+public class Kalkulator {
+    public int bagi(int pembilang, int penyebut) {
+        return pembilang / penyebut;
+    }
+}
---
+++ Modul-11/PreTest/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Kalkulator kalkulator = new Kalkulator();
+
+        try {
+            System.out.print("Masukkan angka pembilang: ");
+            int pembilang = input.nextInt();
+
+            System.out.print("Masukkan angka penyebut: ");
+            int penyebut = input.nextInt();
+
+            int hasil = kalkulator.bagi(pembilang, penyebut);
+
+            System.out.println("Hasil pembagian: " + hasil);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Penyebut tidak boleh bernilai 0");
+        } catch (InputMismatchException e) {
+            System.out.println("Error: Input harus berupa angka");
+        } finally {
+            input.close();
+            System.out.println("Proses kalkulasi selesai dan resource memory telah dibersihkan");
+        }
+    }
+}
